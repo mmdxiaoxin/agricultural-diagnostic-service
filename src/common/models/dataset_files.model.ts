7aefb055cbff414b6fcdbf3c779bc9656b1368e1
小新 <782446723@qq.com>
2025-03-01 14:01:51 +0800

    fix: 主键丢失

--- src/common/models/dataset_files.model.ts
+++ src/common/models/dataset_files.model.ts
@@ -7,16 +7,19 @@ import {
   Model,
   Table,
   UpdatedAt,
+  PrimaryKey,
 } from 'sequelize-typescript';
 import { Datasets } from './datasets.model';
 import { File } from './file.model';
 
 @Table({ tableName: 'dataset_files', timestamps: true })
 export class DatasetFiles extends Model<DatasetFiles> {
+  @PrimaryKey
   @ForeignKey(() => Datasets)
   @Column(DataType.INTEGER)
   dataset_id!: number;
 
+  @PrimaryKey
   @ForeignKey(() => File)
   @Column(DataType.INTEGER)
   file_id!: number;
