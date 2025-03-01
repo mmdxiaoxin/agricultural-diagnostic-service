090c6d74947ba78274c82484941b39efe6ab2513
小新 <782446723@qq.com>
2025-03-01 16:00:50 +0800

    fix: 错误的用法，重置

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,44 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { LoginDto, RegisterDto } from 'src/common/dto/auth.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  // 注册
-  @Post('register')
-  async registerUser(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto);
-  }
-
-  // 登录
-  @Post('login')
-  async loginUser(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
-  }
-
-  // 激活
-  @Get('verify/:token')
-  async verifyUser(@Param('token') token: string) {
-    return this.authService.verify(token);
-  }
-
-  // 获取角色字典（需要认证）
-  @Get('role-dict')
-  async getRoleDict() {
-    return this.authService.getRoleDict();
-  }
-
-  // 获取按钮权限（需要认证）
-  @Get('buttons')
-  async getButtons() {
-    return this.authService.getButtons();
-  }
-
-  // 获取路由信息（需要认证）
-  @Get('route')
-  async getRoute() {
-    return this.authService.getRoute();
-  }
-}
+export class AuthController {}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from 'src/common/models/user.model';
-import { Role } from 'src/common/models/role.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User, Role])],
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService],
 })
 export class UserModule {}
