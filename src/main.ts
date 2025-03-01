8edd06a77e781dbc172ae999dcbee6fce23f7a45
小新 <782446723@qq.com>
2025-03-01 17:23:56 +0800

    feat: 使用过滤器捕获异常

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+
+  const logger = new Logger();
+  app.useGlobalFilters(new AllExceptionFilter(logger, httpAdapter));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
