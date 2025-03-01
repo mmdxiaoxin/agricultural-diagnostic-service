102f33c12809ec7fe413fcca35b98ce36fc82e0f
小新 <782446723@qq.com>
2025-03-01 19:55:24 +0800

    feat: 更好的注册响应

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { RegisterDto } from './auth.dto';
 import { AuthService } from './auth.service';
-
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @HttpCode(HttpStatus.CREATED)
   async register(@Body() dto: RegisterDto) {
     const { email, password } = dto;
-    return this.authService.register(email, password);
+    this.authService.register(email, password);
+    return {
+      code: 201,
+      data: null,
+      message: '注册成功',
+    };
   }
 }
