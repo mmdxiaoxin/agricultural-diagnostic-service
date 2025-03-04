a442d1f488397023f32b9ea85088eff13357afa3
小新 <782446723@qq.com>
2025-03-04 14:13:26 +0800

    docs: swagger

--- src/modules/ai-service/dto/create-ai-service.dto.ts
+++ src/modules/ai-service/dto/create-ai-service.dto.ts
@@ -1,21 +1,44 @@
-import { IsString, IsOptional, IsEnum } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateAiServiceDto {
   @IsString()
+  @ApiProperty({
+    description: '服务名称',
+    example: '病害智能诊断服务',
+  })
+  @IsNotEmpty()
   serviceName: string; // 服务名称
 
   @IsOptional()
   @IsString()
-  serviceType: string; // 服务类型
+  @ApiProperty({
+    description: '服务类型',
+    example: 'diagnosis',
+  })
+  serviceType?: string; // 服务类型
 
   @IsOptional()
   @IsString()
-  description: string; // 服务描述
+  @ApiProperty({
+    description: '服务描述',
+    example: '用于病害智能诊断的服务',
+  })
+  description?: string; // 服务描述
 
   @IsEnum(['active', 'inactive', 'under_maintenance'])
-  status: 'active' | 'inactive' | 'under_maintenance'; // 服务状态
+  @IsOptional()
+  @ApiProperty({
+    description: '服务状态',
+    example: 'active',
+  })
+  status?: 'active' | 'inactive' | 'under_maintenance'; // 服务状态
 
   @IsOptional()
   @IsString()
-  endpointUrl: string; // 服务的访问URL
+  @ApiProperty({
+    description: '服务的访问URL',
+    example: 'http://localhost:5001',
+  })
+  endpointUrl?: string; // 服务的访问URL
 }
--- src/modules/ai-service/dto/update-ai-service.dto.ts
+++ src/modules/ai-service/dto/update-ai-service.dto.ts
@@ -1,23 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional, IsEnum } from 'class-validator';
 
 export class UpdateAiServiceDto {
-  @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: '服务名称',
+    example: '病害智能诊断服务',
+  })
+  @IsOptional()
   serviceName?: string; // 服务名称
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: '服务类型',
+    example: 'diagnosis',
+  })
   serviceType?: string; // 服务类型
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: '服务描述',
+    example: '用于病害智能诊断的服务',
+  })
   description?: string; // 服务描述
 
-  @IsOptional()
   @IsEnum(['active', 'inactive', 'under_maintenance'])
+  @IsOptional()
+  @ApiProperty({
+    description: '服务状态',
+    example: 'active',
+  })
   status?: 'active' | 'inactive' | 'under_maintenance'; // 服务状态
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: '服务的访问URL',
+    example: 'http://localhost:5001',
+  })
   endpointUrl?: string; // 服务的访问URL
 }
