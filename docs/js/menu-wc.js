2ca5241d601887cc3feca33df9bcf7eaff4615a7
小新 <782446723@qq.com>
2025-03-04 15:47:31 +0800

    docs: 自动生成文档

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">agricultural-diagnostic-service documentation</a>
+                    <a href="index.html" data-type="index-link">病害智能诊断系统服务文档</a>
                 </li>
 
                 <li class="divider"></li>
