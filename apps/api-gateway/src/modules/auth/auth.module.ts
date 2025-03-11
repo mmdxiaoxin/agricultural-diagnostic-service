8875ffc3298280b215c25be8331164c83df6a865
小新 <782446723@qq.com>
2025-03-11 14:43:09 +0800

    refactor: 精简api-gateway

--- apps/api-gateway/src/modules/auth/auth.module.ts
+++ apps/api-gateway/src/modules/auth/auth.module.ts
@@ -1,32 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { PassportModule } from '@nestjs/passport';
-import { ConfigEnum } from '@shared/enum/config.enum';
 import {
   AUTH_SERVICE_NAME,
   AUTH_SERVICE_PORT,
 } from 'config/microservice.config';
-import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
-    UserModule,
-    PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          secret: configService.get<string>(ConfigEnum.SECRET),
-          signOptions: {
-            expiresIn: '1d', // 默认一天
-          },
-        };
-      },
-      inject: [ConfigService],
-    }),
     ClientsModule.register([
       {
         name: AUTH_SERVICE_NAME,
@@ -36,6 +17,5 @@ import { AuthController } from './auth.controller';
     ]),
   ],
   controllers: [AuthController],
-  providers: [],
 })
 export class AuthModule {}
