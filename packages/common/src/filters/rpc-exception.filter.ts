c36f111086e6bb888bf6831e349ae59defa03ced
小新 <782446723@qq.com>
2025-03-11 21:30:54 +0800

    feat: 全部的微服务异常捕获

--- packages/common/src/filters/rpc-exception.filter.ts
+++ packages/common/src/filters/rpc-exception.filter.ts
@@ -4,33 +4,58 @@ import {
   HttpStatus,
   RpcExceptionFilter,
 } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
 import { Observable, throwError } from 'rxjs';
+import { RpcException } from '@nestjs/microservices';
 
-@Catch(RpcException)
-export class CustomRpcExceptionFilter
-  implements RpcExceptionFilter<RpcException>
-{
-  catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
-    let errorResponse:
-      | { code?: number; message?: string; data?: any }
-      | string = exception.getError();
+@Catch()
+export class CustomRpcExceptionFilter implements RpcExceptionFilter<any> {
+  catch(exception: any, host: ArgumentsHost): Observable<any> {
+    let errorResponse: { code: number; message: string; data: any } = {
+      code: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal server error',
+      data: null,
+    };
 
-    // 兼容 `string` 和 `object` 两种异常返回格式
-    if (typeof errorResponse === 'string') {
+    // 如果异常是 RpcException，调用 getError() 获取信息
+    if (exception instanceof RpcException) {
+      const rpcError: any = exception.getError();
+      if (typeof rpcError === 'string') {
+        errorResponse = {
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: rpcError,
+          data: null,
+        };
+      } else if (typeof rpcError === 'object' && rpcError !== null) {
+        errorResponse = {
+          code: rpcError?.code || HttpStatus.INTERNAL_SERVER_ERROR,
+          message: rpcError?.message || 'Internal server error',
+          data: rpcError?.data || null,
+        };
+      }
+    }
+    // 如果异常是 HttpException 或其他 Error 类型
+    else if (exception instanceof Error) {
       errorResponse = {
         code: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: errorResponse,
+        message: exception.message,
         data: null,
       };
     }
+    // 如果异常为字符串或其他类型
+    else if (typeof exception === 'string') {
+      errorResponse = {
+        code: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: exception,
+        data: null,
+      };
+    } else {
+      errorResponse = {
+        code: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Unknown error',
+        data: exception,
+      };
+    }
 
-    const formattedError = {
-      code: errorResponse.code || HttpStatus.INTERNAL_SERVER_ERROR,
-      message: errorResponse.message || 'Internal server error',
-      data: errorResponse.data || null,
-    };
-
-    return throwError(() => formattedError);
+    return throwError(() => errorResponse);
   }
 }
