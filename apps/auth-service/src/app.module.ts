6cea17695a0709ab864066ef4e9baed924ce7583
小新 <782446723@qq.com>
2025-03-12 22:11:03 +0800

    refactor: 简化数据库注册

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,8 +1,7 @@
+import { DatabaseModule } from '@app/database';
 import { MetricsModule } from '@app/metrics';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigEnum } from '@shared/enum/config.enum';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { MenuModule } from './modules/menu/menu.module';
 
@@ -15,22 +14,7 @@ import { MenuModule } from './modules/menu/menu.module';
         `.env.${process.env.NODE_ENV || 'development'}.local`,
       ],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        ({
-          type: configService.get(ConfigEnum.DB_TYPE),
-          host: configService.get(ConfigEnum.DB_HOST),
-          port: configService.get(ConfigEnum.DB_PORT),
-          username: configService.get(ConfigEnum.DB_USERNAME),
-          password: configService.get(ConfigEnum.DB_PASSWORD),
-          database: configService.get(ConfigEnum.DB_DATABASE),
-          autoLoadEntities: true, // 自动加载实体
-          synchronize: configService.get(ConfigEnum.DB_SYNC),
-          logging: process.env.NODE_ENV === 'development',
-        }) as TypeOrmModuleOptions,
-    }),
+    DatabaseModule.register(),
     AuthModule,
     MenuModule,
     MetricsModule,
