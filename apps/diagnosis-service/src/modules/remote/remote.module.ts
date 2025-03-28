b18a1939320ba3bbfb70afcaa82d0a566e315bba
mmdxiaoxin <782446723@qq.com>
2025-03-28 19:35:54 +0800

    fix: 新实体未导入

--- apps/diagnosis-service/src/modules/remote/remote.module.ts
+++ apps/diagnosis-service/src/modules/remote/remote.module.ts
@@ -1,12 +1,15 @@
 import { DatabaseModule } from '@app/database';
-import { RemoteService, RemoteInterface } from '@app/database/entities';
+import { RemoteInterface, RemoteService } from '@app/database/entities';
+import { RemoteConfig } from '@app/database/entities/remote-config.entity';
 import { Module } from '@nestjs/common';
 import { RemoteServiceController } from './remote.controller';
 import { RemoteInterfaceService } from './services/remote-interface.service';
 import { RemoteServiceService } from './services/remote.service';
 
 @Module({
-  imports: [DatabaseModule.forFeature([RemoteService, RemoteInterface])],
+  imports: [
+    DatabaseModule.forFeature([RemoteService, RemoteInterface, RemoteConfig]),
+  ],
   providers: [RemoteServiceService, RemoteInterfaceService],
   controllers: [RemoteServiceController],
 })
