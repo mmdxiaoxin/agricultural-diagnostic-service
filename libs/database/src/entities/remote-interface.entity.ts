80b2bc0e1397e649e5a3e5334aefe8ff69cbb16d
mmdxiaoxin <782446723@qq.com>
2025-04-14 14:11:59 +0800

    fix: 服务接口描述可为空

--- libs/database/src/entities/remote-interface.entity.ts
+++ libs/database/src/entities/remote-interface.entity.ts
@@ -33,7 +33,7 @@ export class RemoteInterface extends BaseEntity {
   @Column({ type: 'varchar', length: 255 })
   name: string; // 接口名称
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   description: string; // 接口描述
 
   @Column({ type: 'varchar', length: 255 })
