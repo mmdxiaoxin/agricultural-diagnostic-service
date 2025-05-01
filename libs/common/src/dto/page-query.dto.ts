237304658e67e0b09bc9de9bf15e1113cb6e12a2
mmdxiaoxin <782446723@qq.com>
2025-05-01 17:44:18 +0800

    fix: 更新PageQueryDto默认值并移除冗余转换

--- libs/common/src/dto/page-query.dto.ts
+++ libs/common/src/dto/page-query.dto.ts
@@ -1,21 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, Min } from 'class-validator';
-import { Transform, Type } from 'class-transformer';
 
 export class PageQueryDto {
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
-  @Transform(({ value }) => (value ? Number(value) : 1))
   @ApiProperty({ description: '页码', default: 1 })
-  page: number;
+  page: number = 1;
 
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
-  @Transform(({ value }) => (value ? Number(value) : 10))
   @ApiProperty({ description: '每页条数', default: 10 })
-  pageSize: number;
+  pageSize: number = 10;
 }
