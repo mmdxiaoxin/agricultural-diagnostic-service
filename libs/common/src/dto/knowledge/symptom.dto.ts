6a7166bfe5573cbb05308ccb9f06a01f92c5f522
mmdxiaoxin <782446723@qq.com>
2025-05-06 14:04:52 +0800

    docs: 更新症状DTO文档，添加type、nullable属性及示例

--- libs/common/src/dto/knowledge/symptom.dto.ts
+++ libs/common/src/dto/knowledge/symptom.dto.ts
@@ -2,12 +2,28 @@ import { ApiProperty } from '@nestjs/swagger';
 import { BaseDto } from '../base.dto';
 
 export class SymptomDto extends BaseDto {
-  @ApiProperty({ description: '症状描述', required: true })
+  @ApiProperty({
+    type: String,
+    description: '症状描述',
+    required: true,
+    example: '叶片上出现黄色或橙色的锈斑，后期病斑扩大并产生锈色孢子',
+  })
   description: string;
 
-  @ApiProperty({ description: '图片URL', required: false })
+  @ApiProperty({
+    description: '图片URL',
+    example: 'https://example.com/image.jpg',
+    required: false,
+    nullable: true,
+    type: String,
+  })
   imageUrl?: string;
 
-  @ApiProperty({ description: '生长阶段', required: false })
-  stage?: string;
+  @ApiProperty({
+    description: '生长阶段',
+    example: '发芽期、幼苗期、分枝期、开花期、结荚期、鼓粒期和成熟期',
+    required: true,
+    type: String,
+  })
+  stage: string;
 }
