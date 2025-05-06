98fb9a0361691d04bf73844602d0d23f196ca8c6
mmdxiaoxin <782446723@qq.com>
2025-05-06 15:34:02 +0800

    feat: 将文件服务模块中的TypeOrmModule替换为DatabaseModule，简化数据库注册

--- apps/file-service/src/modules/dataset/dataset.module.ts
+++ apps/file-service/src/modules/dataset/dataset.module.ts
@@ -1,11 +1,11 @@
+import { DatabaseModule } from '@app/database';
 import { Dataset, FileEntity } from '@app/database/entities';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatasetController } from './dataset.controller';
 import { DatasetService } from './dataset.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Dataset, FileEntity])],
+  imports: [DatabaseModule.forFeature([Dataset, FileEntity])],
   providers: [DatasetService],
   controllers: [DatasetController],
 })
--- apps/file-service/src/modules/file/file.module.ts
+++ apps/file-service/src/modules/file/file.module.ts
@@ -1,27 +1,12 @@
+import { DatabaseModule } from '@app/database';
 import { FileEntity } from '@app/database/entities';
 import { FileOperationService } from '@app/file-operation/file-operation.service';
-import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([FileEntity]),
-    BullModule.registerQueue({
-      name: 'file-delete',
-      defaultJobOptions: {
-        attempts: 3,
-        backoff: {
-          type: 'exponential',
-          delay: 1000,
-        },
-        removeOnComplete: true,
-        removeOnFail: false,
-      },
-    }),
-  ],
+  imports: [DatabaseModule.forFeature([FileEntity])],
   controllers: [FileController],
   providers: [FileService, FileOperationService],
   exports: [FileService],
