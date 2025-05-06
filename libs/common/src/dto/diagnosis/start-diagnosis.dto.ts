41280f9472579dbe413445413bab4fb35ae31ffc
mmdxiaoxin <782446723@qq.com>
2025-05-06 12:27:14 +0800

    docs: 诊断参数swagger补充

--- libs/common/src/dto/diagnosis/start-diagnosis.dto.ts
+++ libs/common/src/dto/diagnosis/start-diagnosis.dto.ts
@@ -1,13 +1,24 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class StartDiagnosisDto {
   @IsNotEmpty({ message: 'serviceId 不能为空' })
   @Transform(({ value }) => Number(value))
   @IsNumber({}, { message: 'serviceId 必须为数字' })
+  @ApiProperty({
+    description: '服务ID',
+    type: Number,
+    example: 5,
+  })
   serviceId: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({
+    description: '配置ID',
+    type: Number,
+    example: 107,
+  })
   configId: number;
 }
