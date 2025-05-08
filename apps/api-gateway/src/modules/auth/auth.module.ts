74504d7bc8f3b8e84be407a00664b6299cc2daa4
mmdxiaoxin <782446723@qq.com>
2025-05-08 21:30:53 +0800

    feat: 优化认证服务的客户端注册配置，增加连接选项以提升服务稳定性和性能

--- apps/api-gateway/src/modules/auth/auth.module.ts
+++ apps/api-gateway/src/modules/auth/auth.module.ts
@@ -13,11 +13,21 @@ import { JwtStrategy } from './auth.strategy';
 @Module({
   imports: [
     PassportModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_SERVICE_NAME,
-        transport: Transport.TCP,
-        options: { host: AUTH_SERVICE_HOST, port: AUTH_SERVICE_TCP_PORT },
+        useFactory: () => ({
+          transport: Transport.TCP,
+          options: {
+            host: AUTH_SERVICE_HOST,
+            port: AUTH_SERVICE_TCP_PORT,
+            keepalive: true,
+            keepaliveInitialDelay: 15000,
+            maxConnections: 20,
+            maxRetries: 2,
+            retryDelay: 500,
+          },
+        }),
       },
     ]),
   ],
