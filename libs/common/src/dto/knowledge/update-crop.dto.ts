a1a4ab27c7fbe7235a421eb6416647560cce796f
mmdxiaoxin <782446723@qq.com>
2025-05-10 10:08:59 +0800

    feat: 在作物创建和更新 DTO 中添加多个可选字段，包括作物类型、图片、别名、描述、产地、生长习性、生长周期、适宜种植区域、季节和土壤

--- libs/common/src/dto/knowledge/update-crop.dto.ts
+++ libs/common/src/dto/knowledge/update-crop.dto.ts
@@ -16,4 +16,54 @@ export class UpdateCropDto {
   @IsString()
   @ApiProperty({ description: '生长阶段', required: false })
   growthStage?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '作物类型', required: false })
+  cropType?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '作物图片', required: false })
+  imageUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '作物别名', required: false })
+  alias?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '作物描述', required: false })
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '产地', required: false })
+  origin?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '作物生长习性', required: false })
+  growthHabits?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '作物生长周期', required: false })
+  growthCycle?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '适宜种植区域', required: false })
+  suitableArea?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '适宜种植季节', required: false })
+  suitableSeason?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ description: '适宜种植土壤', required: false })
+  suitableSoil?: string;
 }
